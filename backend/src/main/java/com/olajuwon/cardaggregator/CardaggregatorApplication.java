915bcd212d5e99b4f5c0c228d5ea84aef662f97c
Juwon <juwon@Juwons-Macbook.local>
2026-06-01 23:14:23 -0400

    initial project scaffold - spring and react

---
+++ backend/src/main/java/com/olajuwon/cardaggregator/CardaggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.olajuwon.cardaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardaggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardaggregatorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/olajuwon/cardaggregator/CardaggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olajuwon.cardaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardaggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
